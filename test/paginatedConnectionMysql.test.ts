6f56764eb2b81ef9926cabd4b303b696e623084b
Davide Arena <d.arena@treedom.net>
2024-06-11 12:10:05 +0200

    fix: handle no props.first arg provided when calculating hasNextPage

--- test/paginatedConnectionMysql.test.ts
+++ test/paginatedConnectionMysql.test.ts
@@ -422,7 +422,7 @@ tap.test(
   }
 )
 
-tap.only(
+tap.test(
   'should return first page when not valid input.after cursor provided',
   async (t) => {
     const dataLoaderItems = [
@@ -495,6 +495,88 @@ tap.only(
           cursor: encodeCursor({
             node: item,
             getCursor: () => ({ after: item.id }),
+          }),
+        }
+      }),
+      {}
+    )
+  }
+)
+
+tap.test(
+  'should return expected items when no pagination input provided and hasNextPage should be true',
+  async (t) => {
+    const dataLoaderItems = [
+      {
+        id: randomUUID(),
+        name: 'name1',
+        premium: true,
+      } as MysqlDoc,
+      {
+        id: randomUUID(),
+        name: 'name2',
+        premium: true,
+      } as MysqlDoc,
+      {
+        id: randomUUID(),
+        name: 'name3',
+        premium: true,
+      } as MysqlDoc,
+      {
+        id: randomUUID(),
+        name: 'name4',
+        premium: true,
+      } as MysqlDoc,
+      {
+        id: randomUUID(),
+        name: 'name5',
+        premium: true,
+      } as MysqlDoc,
+    ]
+
+    const data = await mysqlPaginatedConnection<MysqlDoc>({
+      pagination: {},
+      paginationSafeLimit: 2,
+      countLoader: async () => {
+        return 5
+      },
+      dataLoader: async (props) => {
+        return {
+          edges: dataLoaderItems.slice(0, props.first).map((item) => ({
+            node: item,
+            cursor: props.encodeCursor({
+              node: item,
+              getCursor: () => ({
+                after: item.id,
+              }),
+            }),
+          })),
+        }
+      },
+    })
+
+    t.same(
+      decodeCursor(data.pageInfo.endCursor),
+      {
+        after: dataLoaderItems[1].id,
+      },
+      'should match endCursor content value'
+    )
+
+    t.equal(await data.totalCount(), 5, 'should match totalCount value')
+
+    t.equal(data.pageInfo.hasNextPage, true, 'should match hasNextPage value')
+
+    t.same(
+      data.edges,
+      dataLoaderItems.slice(0, 2).map((item) => {
+        return {
+          node: item,
+          cursor: encodeCursor({
+            node: item,
+            getCursor: () => ({
+              after: item.id,
+            }),
           }),
         }
       }),
