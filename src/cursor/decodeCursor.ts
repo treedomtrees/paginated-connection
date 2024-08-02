0999709c03794535e18c0715bc4857f484f57b77
Davide Arena <d.arena@treedom.net>
2024-08-02 15:51:17 +0200

    feat: add custom cursor type support of number and boolean

--- src/cursor/decodeCursor.ts
+++ src/cursor/decodeCursor.ts
@@ -1,10 +1,21 @@
-export const decodeCursor = <TCursor = { after: string }>(cursor: string) => {
+import { CursorValueType, TCursorValueBase } from '.'
+
+export const decodeCursor = <TCursor = TCursorValueBase>(cursor: string) => {
   try {
     const cursorParams = new URLSearchParams(
       Buffer.from(cursor, 'base64url').toString()
     ).entries()
 
-    return Object.fromEntries(cursorParams) as TCursor
+    const parsedCursor: Record<string, CursorValueType> = {}
+
+    for (const serializedCursorValue of [...cursorParams]) {
+      // Parse cursor value and attach to parse cursor object
+      parsedCursor[serializedCursorValue[0]] = JSON.parse(
+        serializedCursorValue[1]
+      ) // Deserialized value is CursorValueType. We use JSON.parse because could parse all handled primitive types
+    }
+
+    return parsedCursor as TCursor
   } catch (err) {
     return {} as TCursor
   }
